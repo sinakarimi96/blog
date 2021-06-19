a27168fecd2520d934c4c3cbf7d858bcd765351a
sinakarimi96 <ahmad.k1025@gamil.com>
2021-06-19 13:35:01 +0430

    Retrieving posts by similarity

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -34,15 +35,30 @@ class Post(models.Model):
     published = PublishedManager() # Our custom manager.
     def get_absolute_url(self):
         return reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day, self.slug])
-        
+    tags = TaggableManager()
+   
 
 
+    #class Meta:
+    #    ordering = ('-publish',)
+    #def __str__(self):
+    #    return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    
     class Meta:
-        ordering = ('-publish',)
-    def __str__(self):
-        return self.title
-
-
+        ordering = ('created',)
+        def __str__(self):
+            return f'Comment by {self.name} on {self.post}'
 
 
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ahmad.k1025@gmail.com'
+EMAIL_HOST_PASSWORD = '95177131025'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
